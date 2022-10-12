2f135679addf763d1812c6baf973209a0b7e5f98
SebastianoMV <sebastianovisco@gmail.com>
2022-10-12 17:33:29 +0200

    Create Generi

---
+++ Controllers/GenreController.cs
@@ -0,0 +1,30 @@
+﻿using csharp_boolflix.BoolContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_boolflix.Controllers
+{
+    public class GenreController : Controller
+    {
+        readonly Context _context = new();
+        public IActionResult Index()
+        {
+            List<Genre> genres = _context.Genres.ToList();
+
+            return View(genres);
+        }
+
+        public IActionResult Create()
+        {
+            Genre genre = new Genre();
+            return View(genre);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Genre form)
+        {
+            _context.Genres.Add(form);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
